6fb321eb036833678581c00e3b47bcf0f2381064
Muhammad Askar <muhammadaskar74@gmail.com>
2021-11-03 02:06:33 +0700

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '53229972868f67c1e28d5c9e019e9a496246c148',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '53229972868f67c1e28d5c9e019e9a496246c148',
+    ),
+  ),
+);
