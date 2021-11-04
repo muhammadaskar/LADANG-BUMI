a3ef54635d6d8575c7aae1dc3d6ffa7f58fb0480
Muhammad Askar <muhammadaskar74@gmail.com>
2021-11-04 20:56:12 +0700

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -286,4 +286,39 @@
     aos_init();
   });
 
+  $(document).ready(function () {
+    // $(".owl-carousel").owlCarousel();
+    $('.owl-carousel').owlCarousel({
+      loop: true,
+      margin: 20,
+      nav: true,
+      navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+          nav: true
+        },
+        600: {
+          items: 1,
+          nav: true
+        },
+        1000: {
+          items: 1,
+          nav: true,
+          loop: true
+        }
+      }
+    })
+    $('.customNextBtn').click(function () {
+      owl.trigger('next.owl.carousel');
+    })
+    // Go to the previous item
+    $('.customPrevBtn').click(function () {
+      // With optional speed parameter
+      // Parameters has to be in square bracket '[]'
+      owl.trigger('prev.owl.carousel', [300]);
+    })
+  });
+
 })();
